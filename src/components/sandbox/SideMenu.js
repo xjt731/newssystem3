1953fbd0f8f84676c3c7dee3da7c87c80edd9095
gilbert777 <xjt674471772@mail.com>
2023-02-06 22:57:34 +1100

    后端SideMenu

--- src/components/sandbox/SideMenu.js
+++ src/components/sandbox/SideMenu.js
@@ -1,15 +1,65 @@
-import React, { useState } from 'react';
+import React, { useEffect,useState } from 'react';
 import {
-  MenuFoldOutlined,
-  MenuUnfoldOutlined,
+
   UploadOutlined,
   UserOutlined,
   VideoCameraOutlined,
+  AppstoreOutlined, MailOutlined, SettingOutlined,
+
+  ContainerOutlined,
+  DesktopOutlined,
+
+  MenuFoldOutlined,
+  MenuUnfoldOutlined,
+  PieChartOutlined,
 } from '@ant-design/icons';
+import axios from 'axios'
 import { Layout, Menu, theme } from 'antd';
 const { Header, Sider, Content } = Layout;
+
+
+
+
 export default function SideMenu() {
-    return (
+  const [menu, setMenu] = useState([])
+  //第一次渲染画面
+  useEffect(() => {
+    axios.get("http://localhost:3000/rights?_embed=children").then(res => {
+      setMenu(res.data)
+      console.log(menu)
+    })
+  }, [])
+
+  //convertObj为一个方法
+  const convertObj = (label, key, icon, children) =>{
+    return {label, key, icon, children}
+  }
+
+  const iconList = {
+      "/home":<UserOutlined />,
+      "/user-manage": <UserOutlined />,
+      "/user-manage/list": <UserOutlined />,
+      "/right-manage": <UserOutlined />,
+      "/right-manage/role/list": <UserOutlined />,
+      "/right-manage/right/list": <UserOutlined />
+      //.......
+  }
+  function transform(list) {
+    let arr =[]
+    list.forEach((item) => {
+      if (item.children && item.children.length !== 0) {
+        arr.push(convertObj(item.title,item.key, iconList[item.key],  transform(item.children)))
+      } else {
+        if (item.pagepermisson) {
+          arr.push(convertObj(item.title,item.key, iconList[item.key]))
+        }
+      }
+    })
+    console.log(arr)
+    return arr
+  }
+ 
+  return (
         <Sider trigger={null} collapsible >
         <div className="logo" />
         <Menu
@@ -17,23 +67,8 @@ export default function SideMenu() {
           mode="inline"
           //defaultSelectedKeys 类型：string[]
           defaultSelectedKeys={['1']}
-          items={[
-            {
-              key: '1',
-              icon: <UserOutlined />,
-              label: 'nav 1',
-            },
-            {
-              key: '2',
-              icon: <VideoCameraOutlined />,
-              label: 'nav 2',
-            },
-            {
-              key: '3',
-              icon: <UploadOutlined />,
-              label: 'nav 3',
-            },
-          ]}
+          //items	菜单内容	类型：ItemType[]  
+          items={transform(menu)}
         />
       </Sider>
     )
