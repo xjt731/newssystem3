a9fb7d452163a1d8b89e3595bdd40538e97736c3
gilbert777 <xjt674471772@mail.com>
2023-02-15 09:14:07 +1100

    用户列表-添加用户

--- src/views/sandbox/user-manage/UserList.js
+++ src/views/sandbox/user-manage/UserList.js
@@ -16,7 +16,7 @@ export default function UserList() {
     const [regionList, setregionList] = useState([])
     //useRef拿到子组件属性
     const addForm = useRef()
-    
+
 
     useEffect(() => {
         axios.get("http://localhost:3000/users?_expand=role").then(res => {
@@ -51,8 +51,10 @@ export default function UserList() {
         {
             title: '角色名称',
             dataIndex: 'role',
+            //UserList.js:55 Uncaught TypeError: Cannot read properties of undefined (reading 'roleName')
             render: (role) => {
                 return role.roleName
+                //return role?.roleName//如果没有就不读？
             }
         },
         {
@@ -100,8 +102,30 @@ export default function UserList() {
     const deleteMethod = (item) => {
         // console.log(item)
         // 当前页面同步状态 + 后端同步
+        setdataSource(dataSource.filter(data => data.id !== item.id))
 
+        axios.delete(`http://localhost:3000/users/${item.id}`)
 
+    }
+
+    const addFormOK = () => {
+        addForm.current.validateFields().then(value => {
+            // console.log(value)
+
+            setisAddVisible(false)
+
+            //post到后端，生成id，再设置 datasource, 方便后面的删除和更新
+            axios.post(`http://localhost:3000/users`, {
+                ...value,
+                "roleState": true,
+                "default": false,
+            }).then(res => {
+                console.log(res.data)
+                setdataSource([...dataSource, res.data])
+            })
+        }).catch(err => {
+            console.log(err)
+        })
     }
 
     return (
@@ -124,17 +148,7 @@ export default function UserList() {
                 onCancel={() => {
                     setisAddVisible(false)
                 }}
-                onOk={() => {
-                    console.log(addForm.current)
-                        //validateFields 返回示例
-                        addForm.current.validateFields()
-                        .then((values) => {
-                           console.log(values)
-                        })
-                        .catch((errorInfo) => {
-                            console.log(errorInfo)
-                        });
-                }}
+                onOk={() => addFormOK()}
             >
 
                 {/* <Form
