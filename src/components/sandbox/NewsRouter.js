aa49debb7e321385f1828ad3986c1915f2bd4d07
gilbert777 <xjt674471772@mail.com>
2023-02-16 11:48:23 +1100

    路由权限-权限控制

--- src/components/sandbox/NewsRouter.js
+++ src/components/sandbox/NewsRouter.js
@@ -1,7 +1,7 @@
-import React from 'react'
-import Home from '../../views/sandbox/home/Home'
-import Nopermission from '../../views/sandbox/nopermission/Nopermission'
-import RightList from '../../views/sandbox/right-manage/RightList'
+import React, { useEffect, useState } from 'react'
+ import Home from '../../views/sandbox/home/Home'
+ import Nopermission from '../../views/sandbox/nopermission/Nopermission'
+ import RightList from '../../views/sandbox/right-manage/RightList'
 import RoleList from '../../views/sandbox/right-manage/RoleList'
 import UserList from '../../views/sandbox/user-manage/UserList'
 import { Switch, Route, Redirect } from 'react-router-dom'
@@ -10,12 +10,13 @@ import NewsDraft from '../../views/sandbox/news-manage/NewsDraft'
 import NewsCategory from '../../views/sandbox/news-manage/NewsCategory'
 import Audit from '../../views/sandbox/audit-manage/Audit'
 import AuditList from '../../views/sandbox/audit-manage/AuditList'
-import Unpublished from '../../views/sandbox/publish-manage/Unpublished'
-import Published from '../../views/sandbox/publish-manage/Published'
-import Sunset from '../../views/sandbox/publish-manage/Sunset'
+ import Unpublished from '../../views/sandbox/publish-manage/Unpublished'
+ import Published from '../../views/sandbox/publish-manage/Published'
+ import Sunset from '../../views/sandbox/publish-manage/Sunset'
+ import axios from 'axios'
 
-const LocalRouterMap = {
-    "/home":Home,
+ const LocalRouterMap = {
+     "/home":Home,
     "/user-manage/list":UserList,
     "/right-manage/role/list":RoleList,
     "/right-manage/right/list":RightList,
@@ -27,18 +28,49 @@ const LocalRouterMap = {
     "/publish-manage/unpublished":Unpublished,
     "/publish-manage/published":Published,
     "/publish-manage/sunset":Sunset
-}
+ }
 
-export default function NewsRouter() {
-    return (
-        <Switch>
-            <Route path="/home" component={Home} />
-            <Route path="/user-manage/list" component={UserList} />
-            <Route path="/right-manage/role/list" component={RoleList} />
-            <Route path="/right-manage/right/list" component={RightList} />
+ export default function NewsRouter() {
 
-            <Redirect from="/" to="/home" exact />
-            <Route path="*" component={Nopermission} />
-        </Switch>
-    )
-}
+     const [BackRouteList, setBackRouteList] = useState([])
+     useEffect(()=>{
+         Promise.all([
+             axios.get("http://localhost:3000/rights"),
+             axios.get("http://localhost:3000/children"),
+         ]).then(res=>{
+             // console.log(res)
+             setBackRouteList([...res[0].data,...res[1].data])
+             // console.log(BackRouteList)
+         })
+     },[])
+
+     const {role:{rights}} = JSON.parse(localStorage.getItem("token"))
+
+     const checkRoute = (item)=>{
+         return LocalRouterMap[item.key] && item.pagepermisson
+     }
+
+     const checkUserPermission = (item)=>{
+         return rights.includes(item.key)
+     }
+
+     return (
+         <Switch>
+             {
+                 BackRouteList.map(item=>
+                     {
+                         if(checkRoute(item) && checkUserPermission(item)){
+                             return <Route path={item.key} key={item.key} component={LocalRouterMap[item.key]} exact/> 
+                         }
+                         return null
+                     }   
+                 )
+             }
+
+             <Redirect from="/" to="/home" exact />
+             {
+                 BackRouteList.length>0 && <Route path="*" component={Nopermission} />
+             }
+         </Switch>
+     )
+ }
