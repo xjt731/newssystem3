273133253712804613c0c0b09f038211b5d2e969
gilbert777 <xjt674471772@mail.com>
2023-02-18 17:10:58 +1100

    发布管理-自定义hooks-将类似的方法放一起

--- src/views/sandbox/publish-manage/Published.js
+++ src/views/sandbox/publish-manage/Published.js
@@ -1,22 +1,16 @@
-import React,{useEffect, useState} from 'react'
-import axios from 'axios'
 import NewsPublish from '../../../components/publish-manage/NewsPublish'
-export default function Published() {
+import usePublish from '../../../components/publish-manage/usePublish'
+import {Button} from 'antd'
 
-    const {username} = JSON.parse(localStorage.getItem("token"))
-
-    const [dataSource, setdataSource] = useState([])
-    useEffect(() => {
-
-        axios(`http://localhost:3000/news?author=${username}&publishState=2&_expand=category`).then(res=>{
-            // console.log(res.data)
-            setdataSource(res.data)
-        })
-    }, [username])
+export default function Unpublished() {
+    // 1=== 待发布的
+    const {dataSource,handlePublish} = usePublish(1)
 
     return (
         <div>
-            <NewsPublish dataSource={dataSource}></NewsPublish>
+            <NewsPublish dataSource={dataSource} button={(id)=><Button type="primary" onClick={()=>handlePublish(id)}>
+                发布
+            </Button>} ></NewsPublish>
         </div>
     )
 }
